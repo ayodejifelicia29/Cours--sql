b69f08a9111aa78a34d271cdbec5c34959633a6c
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-03-31 12:33:09 +0200

    ok

--- jour1/05-table.sql
+++ jour1/05-table.sql
@@ -16,7 +16,10 @@ CREATE TABLE IF NOT EXISTS etudiant (
 		 nom  VARCHAR (255)
 		 );
 
--- une fois que que la requête est 
+--une fois que la requête est écrite => exécuter 
+-- 1 sur quelle base de données ??
+-- 2 exécuter 
+
 
 -- mots en majuscules sont des mots clé du langage SQL
 
@@ -31,8 +34,28 @@ CREATE TABLE IF NOT EXISTS etudiant (
 -- exécuter la requête SQL 
 -- afficher la table dans la section SQlite Explorer
 
+
+
+-- base de données (stocker) => nodejs + express (3 semaines) => (API) => ajax html appel
+-- $.ajax("https://restcountries.com/v3.1/all")
+
+-- on peut laisser ce code dans 05-table.sql => la requête est stockée dans .sql 
+--  et faire l'exploreur dans le 04-demo.db ?? => ne le touche pas / ne pas écrire dedans / 
+-- tu peux à la rigueur le regarder via les onglets VSCode => code binaire 
+-- langage machine (processeur)
+-- avoir une vision du contenu du fichier 04-demo.sql => utiliser Sqlite Explore
+
 create table if  not exists exo1(
        adresse varchar (255),
 	   ville varchar (255),
 	    nom varchar (255)
 );
+
+-- ne pas mettre de , après la définition de la dernière colonne
+-- pour les requêtes SQL si vous devez exécuter 1 seule requête le ; final est facultatif
+-- les sauts de ligne sont facultatifs, permettent d'aérer le code 
+-- mot clé de SQL en MAJUSCULE et nom table / colonne => minuscule 
+-- mot clé peuvent être écrits en MAJUSCULE ou minuscule 
+CREATE TABLE IF NOT EXISTS exo1 ( adresse VARCHAR(255), ville VARCHAR(255),  nom VARCHAR(255) ) ;
+
+-- une fois que la requête est exécutée => faire une refresh de SQlite Explorer via la bouton flêche qui tourne 
--- jour1/06-type.sql
+++ jour1/06-type.sql
@@ -8,27 +8,67 @@
 
 --- vous allez avoir des catégories => distinguent par le nomber de caractéres quelles contiennent
 
-VARCHAR ()
-TEXT
-LONGTEXT
-BLOG
+VARCHAR (0-255) colonne dont stocker texte maximum de 
+                255 caratères seul type qui dispose de (0-255) =  2^8 = 256
+TEXT  2^16 65 000 lettres
+MEDIUMTEXT  2^24 16 000 000
+LONGTEXT   2^32  4 000 000 000
+BLOB       => très très très grosse chaine de caractères 
 
-----2 les chiffres 
--- chiffres entiers 1 2 3
-TINYINT
-SMALLINT
-MEDIUMINT
-INT
-BIGINT
+-- 2 les chiffres 
+-- chiffres entiers 1 2 3 -2 -8
+TINYINT 2^8 -128 127 => age 
+SMALLINT 2^16 => 65 000 
+MEDIUMINT 2^24 => 16 000 000
+INT  2^32 => 4 000 000 000 
+BIGINT => très grand nombre 
+-- chiffres à virgule 1.4 -10.03
+DECIMAL 100000000000000000000.1
+DECIMAL 1.000000000000000000001
+DECIMAL(60,2) spécial MySQL / sur SQlite DECIMAL
+FLOAT   0 1 
 
-----	chiffres à virgule 1.4 -10.03
-DECIMAL
-FLOAT
 
 --------chiffres spéciaux 
 BOOLEAN
 
 --3 date 
-DATE 
-DATETIME 
-TIME
+DATE  =>  AAAA-MM-JJ
+( 01/12/2023 => date francais non) 
+Attention en sql => 2023-12-01
+DATETIME  AAAA-MM-JJ HH:MM:SS (2023-12-01 12:30:01)
+TIME   HH:MM:SS (12:30:01)
+TIMESTAMP ( MySql non disponible )
+
+
+1/12/2023 => date en français 
+attention en sql =>  "2023-12-01"
+attention en sql =>  "2023-12-01 12:30:01"
+// 01/12/2023 à 12h30 et 1 seconde
+-- MySQL MariaDB 
+CREATE DATABASE demo DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- sqlite => ce concept n'existe pas 
+-- fichier créer des variables ou tu crées csv 
+
+// new Date() // 1er Janvier 1970
+
+// bug de l'an "1999-12-01"
+// bug de l'an "2000-12-01"
+
+// limites des dates => "50100-12-01"
+-- créer une table articles
+--5 colonnes
+-- titre texte max de 255
+-- contenu max de 65 000 lettres
+--auteur texte max de 20 letters
+-- like chiffre entier maximum 4 000 000 000
+-- dt_publication année mois et jour 
+
+
+CREATE TABLE IF NOT EXISTS articles(
+          titre VARCHAR(255),
+          contenu TEXT,
+          auteur VARCHAR (255),
+          like INT ,
+          dt_publication DATE 
+       );
