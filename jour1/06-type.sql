9e775184c7897fede6b55daedcc3400daca60002
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-03-31 11:45:09 +0200

    tout

---
+++ jour1/06-type.sql
@@ -0,0 +1,34 @@
+--en js
+-- dans  une variable on distingue 5 type de base
+-- string/ number/ boolean / array /objet
+
+-- en SQL 
+-- 3 grandes familles de type 
+-- 1 les textes
+
+--- vous allez avoir des catégories => distinguent par le nomber de caractéres quelles contiennent
+
+VARCHAR ()
+TEXT
+LONGTEXT
+BLOG
+
+----2 les chiffres 
+-- chiffres entiers 1 2 3
+TINYINT
+SMALLINT
+MEDIUMINT
+INT
+BIGINT
+
+----	chiffres à virgule 1.4 -10.03
+DECIMAL
+FLOAT
+
+--------chiffres spéciaux 
+BOOLEAN
+
+--3 date 
+DATE 
+DATETIME 
+TIME
